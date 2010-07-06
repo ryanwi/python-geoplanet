e202cdb8b61115ece02eb2785a201f4329cdf33d
Ryan W <rwilliamz@gmail.com>
2010-07-06 01:08:19 -0700

    Clean up

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+# Simple test harness to be run from command-line
+
+
 from geoplanet import *
 
 
-geoplanet = GeoPlanet(appid='.lf0CA3V34GlprmI0No54l93mWlMhLr6TUNN9WK5wFSeY5w2AtnsP2BfBXkjnobgJKUx') #json
-#geoplanet = GeoPlanet(appid='.lf0CA3V34GlprmI0No54l93mWlMhLr6TUNN9WK5wFSeY5w2AtnsP2BfBXkjnobgJKUx', format='xml')
+geoplanet = GeoPlanet(appid='') #json
+#geoplanet = GeoPlanet(appid='', format='xml')
 
 print geoplanet.place(woeid=2507854)
 #print geoplanet.continents()
