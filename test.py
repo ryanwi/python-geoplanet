da5d0ecc888c7ee92d19e2d40d937f0b384bf9dd
Ryan W <rwilliamz@gmail.com>
2010-07-07 20:17:22 -0700

    cleanup

--- test.py
+++ test.py
@@ -15,14 +15,21 @@ geoplanet = GeoPlanet(appid='.lf0CA3V34GlprmI0No54l93mWlMhLr6TUNN9WK5wFSeY5w2Atn
 #print geoplanet.place.woeid_2507854.children()
 
 # Get all US states
-#print geoplanet.states.US()
+#print geoplanet.states.US(view='long')
 #print geoplanet.place.woeid_23424977.children(filter='type(8);count=51')
+
 
 
 # Get counties in a state
 #print geoplanet.counties.OR()
 #print geoplanet.ADMIN2S.{ADMIN1S}
-print place/[state woeid]/children(filter='type(9)')
+#print place/[state woeid]/children(filter='type(9)')
+
+
+#2347596
+response = geoplanet.place.woeid_2347563.descendants(filter='type(7);count=1')
+print response
+print response["places"]["total"]
 
 
 # Get cities in a county
